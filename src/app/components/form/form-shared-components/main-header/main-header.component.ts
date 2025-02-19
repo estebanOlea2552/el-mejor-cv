1e45610ab390d20d269869c72071b7f9a9177e16
Esteban <estebanolea2306@gmail.com>
2025-02-19 18:56:00 -0300

    Primera versión incompleta de landing page con la paleta de colores original

--- src/app/components/form/form-shared-components/main-header/main-header.component.ts
+++ src/app/components/form/form-shared-components/main-header/main-header.component.ts
@@ -5,10 +5,16 @@ import { Component, Input, OnInit } from '@angular/core';
   template: `
     <header class="header">
         <div class="logo-container">
-          <h1>CV-Maker</h1>
+          <img src="../../../../../assets/images/logo.svg" alt="">
+          <h1>El Mejor <span>CV</span></h1>
         </div>
         <div class="container">
-
+          <a
+          href='https://cafecito.app/3steban-0lea_dev'
+          rel='noopener'
+          target='_blank'>
+            <img srcset='https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_1.png' alt='Invitame un café en cafecito.app' />
+          </a>
         </div>
     </header>
   `,
@@ -20,14 +26,28 @@ import { Component, Input, OnInit } from '@angular/core';
         display: grid;
         grid-template-columns: 15% auto;
         background-color: var(--marian-blue);
+        padding-left: 5%;
+        padding-right: 5%;
       }
       .logo-container {
-        color: var(--white);
-        width: 100%;
-        height: 100%;
+        height: 9vh;
+        width: auto;  
+        box-sizing: border-box;
         display: flex;
-        justify-content: center;
         align-items: center;
+      }
+      .logo-container img {
+        max-height: 100%;
+        border-radius: 1rem;
+        padding-left: 1rem;
+      }
+      .logo-container h1 {
+        color: #fff;
+        margin-top: 1rem;
+        font-weight: 600;
+        span {
+          color: var(--tea-green);
+        }
       }
       .container {
         width: 100%;
